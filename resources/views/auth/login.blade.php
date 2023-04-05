e3787e3fe58115054ec4331b8715faa6d5ffd6ec
sienadicon <ascii.siena@gmail.com>
2023-04-05 17:46:01 +0800

    login ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,15 +3,15 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+        <div class="col-md-8 cntr">
+            <div class="card" style="width: 75%;">
+                <div class="card-header secondary-bg-color cntr" style="height: 80px; font-size: 25px;">{{ __('LOGIN TO WEBSITE') }}</div>
 
-                <div class="card-body">
+                <div class="card-body primary-bg-color" style="height: 300px;">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="row mb-3">
+                        <div class="row mb-3 mt-4">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
@@ -52,9 +52,9 @@
                         </div>
 
                         <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                            <div class="col-md-8 offset-md-4 ml-100 mt-4">
+                                <button type="submit" class="btn btn-primary" style="width: 100px;">
+                                    {{ __('LOGIN') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
