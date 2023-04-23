e280a0c1364bee03d535f0e32b3b5e3f70fc8cd8
Paul Liboon <proliboon@gmail.com>
2023-04-24 01:07:30 +0800

    migration for clients table

---
+++ database/migrations/2023_04_24_021127_create_clients_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('client_name');
+            $table->string('address');
+            $table->integer('rate_per_hour', false, true);
+            $table->integer('ot_rate_per_hour', false, true);
+            $table->string('abn');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
