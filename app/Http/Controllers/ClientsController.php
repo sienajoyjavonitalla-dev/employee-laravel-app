654589e59a77b6857cc7f740aae49cec756f159d
Paul Liboon <proliboon@gmail.com>
2023-05-16 22:46:59 +0800

    add auth middleware to existing controllers

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -8,6 +8,11 @@ use DataTables;
 
 class ClientsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index(Request $request)
     {
         if ($request->ajax()) {
