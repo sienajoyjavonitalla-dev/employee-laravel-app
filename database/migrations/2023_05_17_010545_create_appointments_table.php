5784f72123e0b9776c2fe210ec0349f6a9a76631
Paul Liboon <proliboon@gmail.com>
2023-05-24 00:08:03 +0800

    migration for appointments table

---
+++ database/migrations/2023_05_17_010545_create_appointments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->datetime('start_time');
+            $table->datetime('finish_time');
+            $table->string('title');
+            $table->longText('comments')->nullable();
+            $table->foreignId('job_id');
+            $table->foreignId('client_id');
+            $table->foreignId('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
