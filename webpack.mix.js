56db7dd3a12eec5acae7f84317b5c38b1b3bece7
Paul Liboon <proliboon@gmail.com>
2023-06-28 01:58:46 +0800

    webpack mix update

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,5 +15,6 @@ mix.js('resources/js/app.js', 'public/js')
     .css('resources/css/app.css', 'public/css')
     .css('resources/css/custom.css', 'public/css')
     .css('resources/css/sidebar-menu.css', 'public/css')
+    .css('resources/css/pusherchat.css', 'public/css')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
