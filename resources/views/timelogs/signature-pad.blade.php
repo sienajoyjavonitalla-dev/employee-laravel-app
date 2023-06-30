f3a4824abd79ca93f51490ad5b5ebee5a84b712d
Paul Liboon <proliboon@gmail.com>
2023-06-30 12:17:06 +0800

    signature fix

--- resources/views/timelogs/signature-pad.blade.php
+++ resources/views/timelogs/signature-pad.blade.php
@@ -1,27 +1,12 @@
 
-<!DOCTYPE html>
-<html>
-<head>
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> 
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-<style>
-.kbw-signature { width: 100%; height: 180px;}
-#signaturePad canvas{
-width: 100% !important;
-height: auto;
-}
-</style>
-</head>
-<body>
-<script type="text/javascript" src="http://keith-wood.name/js/jquery.signature.js"></script>
-<link rel="stylesheet" type="text/css" href="http://keith-wood.name/css/jquery.signature.css">
 <script type="text/javascript">
-var signaturePad = $('#signaturePad').signature({syncField: '#signature64', syncFormat: 'PNG'});
-$('#clear').click(function(e) {
-e.preventDefault();
-signaturePad.signature('clear');
-$("#signature64").val('');
-});
+
+    var signaturePad = $('#signaturePad').signature({syncField: '#signature64', syncFormat: 'PNG'});
+
+    $('#clear').click(function(e) {
+        e.preventDefault();
+        signaturePad.signature('clear');
+        $("#signature64").val('');
+    });
+
 </script>
-</body>
-</html>
