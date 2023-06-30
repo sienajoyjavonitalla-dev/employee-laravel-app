1cb1535589cfdf2b48952d0acd7b3c5742ef3a88
Paul Liboon <proliboon@gmail.com>
2023-06-30 10:34:33 +0800

    login card styling fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,19 +2,19 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center mt-4">
         <div class="col-md-8 cntr">
             <div class="card">
-                <div class="card-header secondary-bg-color cntr" style="height: 80px; font-size: 25px;">{{ __('UPRISE LOGIN') }}</div>
+                <div class="card-header secondary-bg-color cntr"><strong>{{ __('UPRISE LOGIN') }}</strong></div>
 
-                <div class="card-body primary-bg-color" style="height: 300px;">
+                <div class="card-body primary-bg-color">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="row mb-3 mt-4">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                        <div class="row">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -25,10 +25,10 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
+                        <div class="row mt-3">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
@@ -39,8 +39,9 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
+                        <div class="row pt-2 pl-1">
+                            <div class="col-md-6"></div>
+                            <div class="col-md-6">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -51,18 +52,23 @@
                             </div>
                         </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-6">
+                        <div class="row mt-4">
+                            <div class="col-md-4">
                                 <button type="submit" class="btn btn-primary" style="width: 100px;">
                                     {{ __('LOGIN') }}
                                 </button>
-
+                            </div>
+                            <div class="col-md-8">
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
                                 @endif
                             </div>
+                        </div>
+
+                        <div class="row">
+                            
                         </div>
                     </form>
                 </div>
