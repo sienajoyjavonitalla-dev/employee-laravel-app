ede026f2db52a164361c10d6105c72ade65fd732
Paul Reuben Liboon <proliboon@gmail.com>
2023-07-12 08:51:11 +0800

    change default sorting order

--- resources/views/logs/footprint.blade.php
+++ resources/views/logs/footprint.blade.php
@@ -42,7 +42,7 @@ $(function () {
             {data: 'description', name: 'description'},
             {data: 'created_at', name: 'created_at'},
         ],
-        order: [[0, 'asc']]
+        order: [[3, 'asc']]
     });
 
 });
