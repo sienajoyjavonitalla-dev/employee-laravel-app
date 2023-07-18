1a58bc58bfc07931e33fa0a2191fdd4526731e04
Paul Liboon <proliboon@gmail.com>
2023-07-18 14:05:55 +0800

    update down function

--- database/migrations/2023_07_06_215348_alter-time_logs.php
+++ database/migrations/2023_07_06_215348_alter-time_logs.php
@@ -26,7 +26,7 @@ class AlterTimeLogs extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('time_logs', function (Blueprint $table) {
             $table->dropColumn('notes');
         });
     }
