dbb5b915aa57e30ea53c04ffd9a77331d9ce1640
sienadicon <ascii.siena@gmail.com>
2023-11-10 13:55:19 +0800

    fix job title data in dttbl

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -371,7 +371,7 @@
             },
             columns: [
                 {data: 'employee', name: 'employee'},
-                {data: 'title', name: 'title'},
+                {data: 'job_title', name: 'job_title'},
                 {data: 'date', name: 'date'},
                 {data: 'hrs_worked', name: 'hrs_worked'},
                 {data: 'with_lunch', name: 'with_lunch'},
